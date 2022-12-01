cdc09821fe2cc170f19931e0f0eae96783c02ef6
arthurcai54 <arthurcai54@gmail.com>
2022-11-30 17:07:50 -0800

    updated app.py file

--- audit_log/app.py
+++ audit_log/app.py
@@ -5,6 +5,7 @@ import json
 import logging
 import logging.config
 from pykafka import KafkaClient
+from flask_cors import CORS, cross_origin
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
@@ -90,6 +91,8 @@ def getNumSalesInfo(index):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("amazonAPI.yaml", strict_validation=True, validate_responses=True)
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 if __name__ == "__main__":
