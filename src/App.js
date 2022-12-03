4d9da52359265a2d999085827c15fc24dac039cd
arthurcai54 <arthurcai54@gmail.com>
2022-12-02 18:59:03 -0800

    updated app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AppStats from './components/AppStats'
 
 function App() {
 
-    const endpoints = ["sale_item", "num_sales"]
+    const endpoints = ["sell", "numsales"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
